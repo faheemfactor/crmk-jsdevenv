590875cc2e384602820e141fea5d48857e090fd7
Faheem Factor <faheemfactor@gmail.com>
2018-03-15 01:32:35 -0700

    test05

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.css'
 import numeral from 'numeral';
 
 
+const year=2019;
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`i would pay ${courseValue} for this awsome course!`); //eslint-disable-line no-console
